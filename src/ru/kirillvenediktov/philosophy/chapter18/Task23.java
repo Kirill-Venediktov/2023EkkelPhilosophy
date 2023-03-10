e1abf9db3785871becb6acb781dfc6d4b83de711
Kirill-Venediktov <Venediktovkirill92@gmail.com>
2023-03-10 21:37:50 +0300

    Task 23 is over

---
+++ src/ru/kirillvenediktov/philosophy/chapter18/Task23.java
@@ -0,0 +1,36 @@
+package ru.kirillvenediktov.philosophy.chapter18;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.nio.CharBuffer;
+import java.nio.channels.FileChannel;
+
+public class Task23 {
+
+    public boolean isPrintable(char c) {
+        return ((c >= '!') && (c <= '~'));
+    }
+
+    public void showCharBuffer(CharBuffer cb) {
+        cb.rewind();
+        while (cb.hasRemaining()) {
+            char c = cb.get();
+            if (isPrintable(c)) {
+                System.out.println(c);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        char[] chars = new char[]{'a', 'b','c','d'};
+        CharBuffer cb = CharBuffer.wrap(chars);
+        Task23 task23 = new Task23();
+        task23.showCharBuffer(cb);
+
+        CharBuffer cb2 = CharBuffer.allocate(6);
+        char[] chars2 = new char[]{(char)0x01, (char)0x07, (char)0x7F,'b','y','e'};
+        cb2.put(chars2);
+        task23.showCharBuffer(cb2);
+
+    }
+}
